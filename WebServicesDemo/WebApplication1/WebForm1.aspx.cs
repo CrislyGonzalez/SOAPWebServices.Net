ac89335a19f66e0a4cca24043b6a1584e3e02fe3
gonzalezcrisly@gmail.com <gonzalezcrisly@gmail>
2018-07-03 16:45:16 -0600

    short example about web services ASP.net

---
+++ WebServicesDemo/WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+	public partial class WebForm1 : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		protected void btnAdd_Click(object sender, EventArgs e)
+		{
+			//public partial class WebService1SoapClient // revisar References.cs ahí es donde esta el servicio
+			CalculatorService.WebService1SoapClient client = 
+				new CalculatorService.WebService1SoapClient();
+			int result = client.Add(Convert.ToInt32(txtFirstNumber.Text), 
+				Convert.ToInt32(txtSecondNumber.Text));
+			lblResult.Text = result.ToString();
+		}
+	}
+}
